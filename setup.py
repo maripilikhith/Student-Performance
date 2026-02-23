ab9eaafdb092d73c54bfd8eb7f481ab10b6311b1
Liki Maripi <your_email@gmail.com>
2026-02-23 19:10:40 +0530

    Initial project structure and requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+name="STUDENT PERFORMANCE",
+version="0.0.1"
+author="Likhith"
+author_email="maripilikhith4@gmail.com",
+packages=find_packages(),
+install_requires=['pandas','seaborn','numpy'] 
+)
